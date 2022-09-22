4b482ab1a0cb5240170ef41eaffddc72252cdd2f
flyingDutchmanLWM7 <suttiphong.ivy@mail.kmutt.ac.th>
2022-09-23 01:44:38 +0700

    change bloodgroup.c

--- bloodgroup.c
+++ bloodgroup.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 void main()
-{ char Name[50], G[5], answer[10];
+{ char Name[40], G[5], answer[10];
   int i;
   do
   {
-    printf("What is A1 Name? ");
-    printf("Change by A2");
+    printf("What is your Name? ");
     scanf("%s", Name);
     do
     {
